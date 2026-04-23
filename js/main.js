11ead7e13bb7667ba74c8247eaa83cccc9e2eb6b
carmenmrandolphfajardo-blip <carmenmrandolphfajardo@gmail.com>
2026-04-22 21:34:47 -0500

    Form styling

--- js/main.js
+++ js/main.js
@@ -16,6 +16,13 @@ const hero = document.querySelector(".hero");
 hero.addEventListener ("click", () => {
 });
 
+const submitBtn = document.getElementById("submitbtn");
+
+submitBtn.addEventListener ("click", () => {
+  alert("Thank you for your message! We will get back to you as soon as possible.");
+});
+
+
 (function() {
  
   window.inputNumber = function(el) {
